b382c3da14206c223393ba6688606c62a6d2763a
Mark <iksnij12@gmail.com>
2024-12-16 18:12:56 +0100

    Javascript yayyy!

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let randomMonster;
+let monster = "";
 fetch("../data/monsters.json")
+
 .then(response => {
     if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
@@ -6,8 +9,28 @@ fetch("../data/monsters.json")
     return response.json();
 })
 .then(data => {
-    console.log(data);
+    const monsters = data.monsters;
+    console.log(monsters);
+    console.log(getRandomMonster(monsters));
 })
 .catch(error => {
     console.error("Error fetching the JSON file:", error);
 });
+
+function getRandomMonster(monsters) {
+    randomMonster = Math.floor(Math.random() * monsters.length);
+    return monsters[randomMonster];
+}
+
+function search() {
+    var text = Document.getElementbyid('search').value;
+    const tr = Document.GetElementsByTagName(tr)
+    for (let i = 1; i < tr.lenght; i++) {
+        if (!tr[i].childeren[1].childeren[1].innerHTML.toLowerCase().includes(text.toLowerCase
+        )) {
+            tr[i].style.display = none;
+        } else {
+            tr[i].style.display = e.e;
+        }
+    }
+}
