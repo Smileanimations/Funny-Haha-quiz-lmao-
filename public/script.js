19aa7190975b9200486c9618f51c005255198cb4
Mark van der Veen <iksnij12@gmail.com>
2026-09-20 00:36:47 +0200

    Made the nav bar a seperate component and styled it

--- public/script.js
+++ public/script.js
@@ -28,6 +28,18 @@ const filterButton = document.getElementById("filterbutton")
 
 guessDivBackground.style.visibility = "hidden";
 
+
+fetch("./modules/nav-bar/nav-bar.html")
+    .then(response => response.text())
+    .then(html => {
+        const navbar = document.createElement('header')
+        navbar.innerHTML = html;
+        body.prepend(navbar);
+    })
+    .catch(error => {
+        console.log("Error fetching component:", error)
+    })
+    
 // Fetch the JSON file
 fetch("./data/monsters.json")
 
